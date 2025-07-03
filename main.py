e7e2f6ce31897f0022f1122913f0115b824bfc05
Gerry Grewall <gobind99@gmail.com>
2025-07-03 13:53:24 -0400

    Create messages array to keep track of user prompts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 load_dotenv()
@@ -8,7 +9,6 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 model = 'gemini-2.0-flash-001'
-contents = ''
 
 def main():       
     args = sys.argv
@@ -21,7 +21,8 @@ def main():
     prompt_content_for_ai_arg = 1 
     contents = args[prompt_content_for_ai_arg] 
 
-    response = client.models.generate_content(model=model, contents=contents) 
+    messages = [types.Content(role="user", parts=[types.Part(text=contents)])]
+    response = client.models.generate_content(model=model, contents=messages) 
     
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")

ba5e551192ac183935fda1ec13f5b87492128f90
Gerry Grewall <gobind99@gmail.com>
2025-07-03 13:35:18 -0400

    Add google gen ai imports and test gemini-2.0-flash-001 model

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from aiagentproject!")
+import os 
+from dotenv import load_dotenv
+from google import genai 
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY") 
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Did my request work?'
+)
+
+
+def main():       
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()

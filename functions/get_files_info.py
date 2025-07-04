5f1cff23cdda261ab2a59753b52fa56e59264f80
Gerry Grewall <gobind99@gmail.com>
2025-07-04 18:48:16 -0400

    Add a function call plan for GenerateContentconfig and add new functiondeclaration for agent to get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory=None): 
     full_path = os.path.abspath(os.path.join(working_directory, directory or "")) 
@@ -21,4 +22,18 @@ def get_files_info(working_directory, directory=None):
         lines.append(f'- {item}: file_size={size} bytes, is_dir={is_dir}') 
 
     return "\n".join(lines) 
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY") 
 client = genai.Client(api_key=api_key)
 
 model = 'gemini-2.0-flash-001'
-system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():       
     args = sys.argv[1:]
@@ -17,17 +32,20 @@ def main():
     if len(args) < 1:
         print("Usage: python3 main.py <PromptForAI> [--verbose]")
         sys.exit(1)
-
-    prompt_content_for_ai_arg = 1 
+    
     contents = args[0] 
 
     messages = [types.Content(role="user", parts=[types.Part(text=contents)])]
     response = client.models.generate_content(
         model=model, 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt))    
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))    
     
-    print(response.text)
+    if len(response.function_calls) == 0:
+        print(response.text)
+    else:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
 
     verbose = "--verbose" in args
 
